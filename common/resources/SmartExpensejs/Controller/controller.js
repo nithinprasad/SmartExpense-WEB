d51116bbc4c0c6382d8672ecc97f606723679da7
Nithin Prasad <nithinprasad549@yahoo.com>
2016-04-03 09:23:09 -0700

    Added Example for angular js
    Added Example to study angular js

---
+++ common/resources/SmartExpensejs/Controller/controller.js
@@ -0,0 +1,5 @@
+smartExpenseApp.controller('smartController', function($scope) {
+    $scope.userName= "nithinprasad59@yahoo.com";
+    $scope.password= "nithin123";
+    $scope.userEmail=$scope.userName;
+});
