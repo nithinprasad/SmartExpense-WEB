281ab44cd571a2cb45b54f8f0d0ff66d0f81d233
Nithin Prasad <nithinprasad549@yahoo.com>
2016-04-03 13:06:03 -0700

    Added Sorting filter in table ,Login Validations
    Added validations

--- common/resources/SmartExpensejs/Controller/controller.js
+++ common/resources/SmartExpensejs/Controller/controller.js
@@ -1,5 +1,79 @@
-smartExpenseApp.controller('smartController', function($scope) {
-    $scope.userName= "nithinprasad59@yahoo.com";
-    $scope.password= "nithin123";
-    $scope.userEmail=$scope.userName;
+smartExpenseApp.controller('smartController', function ($scope, $log) {
+    $scope.userName = "nithinprasad59@yahoo.com";
+    $scope.password = "nithin123";
+
+    $scope.userEmail = $scope.userName;
+
+    $scope.names = [
+        {
+            name: 'Jani'
+            , country: 'Norway'
+        }
+
+        
+        , {
+            name: 'Carl'
+            , country: 'Sweden'
+        }
+
+        
+        , {
+            name: 'Margareth'
+            , country: 'England'
+        }
+
+        
+        , {
+            name: 'Hege'
+            , country: 'Norway'
+        }
+
+        
+        , {
+            name: 'Joe'
+            , country: 'Denmark'
+        }
+
+        
+        , {
+            name: 'Gustav'
+            , country: 'Sweden'
+        }
+
+        
+        , {
+            name: 'Birgit'
+            , country: 'Denmark'
+        }
+
+        
+        , {
+            name: 'Mary'
+            , country: 'England'
+        }
+
+        
+        , {
+            name: 'Kai'
+            , country: 'Norway'
+        }
+  ];
+
+    $scope.sortField = "name";
+    $scope.sortBy = false;
+    $scope.sortColumn = function (column) {
+        $scope.sortField = column;
+        $scope.sortBy = $scope.sortField==(column) ? !$scope.sortBy : false;
+
+    }
+    $scope.getSortClass = function (column) {
+        if ($scope.sortField==column)
+            {
+                return $scope.sortBy ? "glyphicon glyphicon-triangle-bottom" : "glyphicon glyphicon-triangle-top";
+            }
+            
+        return 'glyphicon glyphicon-minus-sign';
+    }
+
+
 });
